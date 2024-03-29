721f24789cbcc2b43a3e3d0e921b5793b7989757
ziauddin-0987 <ziauddink100@gmail.com>
2024-03-29 22:46:29 +0500

    this is my practices

---
+++ index.js
@@ -0,0 +1,24 @@
+function arrFunction() {
+  // this value is arrya
+  const arra = ["zia", "khan", "kamran", "jamal"];
+  arra.find();
+
+  //   arra.push(prompt("print your name in box"));
+  // this is condition a dom
+  const text = document.getElementById("textt").value;
+  // THIS CONDITION  A IF & ELSE
+  if (arra === text) {
+    console.log("your name is success full name");
+  } else {
+    console.log("not  validede your nams");
+  }
+}
+
+function input_fun() {
+  const text = document.getElementById("textt").value;
+  console.log(text);
+}
+// arra.push(prompt("print your name "));
+// console.log(arra);
+
+// // if()
